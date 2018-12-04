d46ea5729b08758f25cf13378f802c8451dd44cf
Isaac Falkenstine <isaac@falkenstine.com>
2018-12-04 14:14:27 -0700

    Added testing for github services

---
+++ spec/services/github_repo_request_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe GithubService do
+  it "exists" do
+    service = GithubService.new
+
+    expect(service).to be_a(GithubService)
+  end
+
+  context "instance methods" do
+    context "#repos" do
+      it "returns a array of 5 repos" do
+        VCR.use_cassette("github_service_spec") do
+          service = GithubService.new
+
+          expect(service.repos).to be_a(Array)
+          expect(service.repos.count).to eq(5)
+          expect(service.repos.first).to have_key(:name)
+          expect(service.repos.first).to have_key(:html_url)
+        end
+      end
+    end
+  end
+end
