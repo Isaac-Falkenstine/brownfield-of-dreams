807d7a6583b198b32302243830907a1ea37d7894
Isaac Falkenstine <isaac@falkenstine.com>
2018-12-04 19:48:49 -0700

    Test passing for github service

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,5 +1,5 @@
 class GithubService
-  def repos
+  def repos_json
     get_json("/user/repos")
   end
 
@@ -13,7 +13,7 @@ class GithubService
 
   def conn
     Faraday.new(:url => 'https://api.github.com') do |faraday|
-      faraday.headers['Authorization'] = ENV["GITHUB_TOKEN"]
+      faraday.headers['Authorization'] = ENV["github_token"]
       faraday.adapter  Faraday.default_adapter
     end
   end
--- spec/services/github_repo_request_spec.rb
+++ spec/services/github_repo_request_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe GithubService do
+describe 'GithubService' do
   it "exists" do
     service = GithubService.new
 
@@ -8,14 +8,14 @@ describe GithubService do
   end
 
   context "instance methods" do
-    context "#repos" do
+    context "#repos_json" do
       it "returns a array of 5 repos" do
         VCR.use_cassette("github_service_spec") do
           service = GithubService.new
 
-          expect(service.repos).to be_a(Array)
-          expect(service.repos.first).to have_key(:name)
-          expect(service.repos.first).to have_key(:html_url)
+          expect(service.repos_json).to be_a(Array)
+          expect(service.repos_json.first).to have_key(:name)
+          expect(service.repos_json.first).to have_key(:html_url)
         end
       end
     end
