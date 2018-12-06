056992a999a0a0f9b6f0d98101f26cbcbaac1646
Isaac Falkenstine <isaac@falkenstine.com>
2018-12-05 21:24:47 -0700

    Passing service test

--- spec/services/github_repo_request_spec.rb
+++ spec/services/github_repo_request_spec.rb
@@ -28,8 +28,21 @@ describe 'GithubService' do
           expect(service_2.repos_json.first).to have_key(:html_url)
           expect(service_2.repos_json.first[:name]).to eq("movie-tracker2")
           expect(service_2.repos_json.first[:html_url]).to eq("https://github.com/amy-r/movie-tracker2")
+        end
+      end
+    end
 
+    context "#following_json" do
+      it "returns the users this user is following" do
+        VCR.use_cassette("github_service_spec", record: :all) do
+          user_1 = create(:user, token: ENV["GITHUB_TOKEN_1"])
+          service_1 = GithubService.new(user_1.token)
 
+          expect(service_1.following_json).to be_a(Array)
+          expect(service_1.following_json.first).to have_key(:login)
+          expect(service_1.following_json.first).to have_key(:html_url)
+          expect(service_1.following_json.first[:login]).to eq("jcasimir")
+          expect(service_1.following_json.first[:html_url]).to eq("https://github.com/jcasimir")
         end
       end
     end
