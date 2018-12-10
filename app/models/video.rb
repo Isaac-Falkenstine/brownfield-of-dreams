7d1a189e36e13599982258a3fc5d66b689780c77
Michael Gatewood <michael@mngatewood.com>
2018-12-10 16:42:53 -0700

    Adds video.position validation and script

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,4 +4,12 @@ class Video < ApplicationRecord
   belongs_to :tutorial
 
   validates_presence_of :position
+
+  def self.assign_nil_position
+    videos = Video.where(position: nil)
+    videos.each do |video|
+      tutorial = Tutorial.find(video.tutorial_id)
+      video.update_attribute(:position, tutorial.videos.maximum(:position) + 1)
+    end
+  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,5 +4,20 @@ describe Video, type: :model do
   describe 'Validations' do
       it { should validate_presence_of :position }
   end
+
+  describe 'Methods' do
+    it 'should assign all nil position values to max plus one' do
+      tutorial = create(:tutorial)
+      video_1 = create(:video, tutorial: tutorial, position: 1)
+      video_2 = create(:video, tutorial: tutorial, position: 2)
+      video_3 = create(:video, tutorial: tutorial)
+      video_3.update_attribute(:position, nil)
+      Video.assign_nil_position
+
+      all_positions = Video.pluck(:position)
+
+      expect(all_positions).to eq([1, 2, 3])
+    end
+  end
 end
 
