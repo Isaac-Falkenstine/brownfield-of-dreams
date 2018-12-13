aa6221f5333ff5ccbe01e90725f2f6a387f56499
Michael Gatewood <michael@mngatewood.com>
2018-12-13 15:14:02 -0700

    Configures mailer for production

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,18 @@ module PersonalProject
     config.middleware.use OmniAuth::Builder do
       provider :github, ENV["GITHUB_ID"], ENV["GITHUB_SECRET"]
     end
+
+    config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.sendgrid.net',
+      port:                 '587',
+      domain:               'herokuapp.com',
+      user_name:            ENV["SENDGRID_USERNAME"],
+      password:             ENV["SENDGRID_PASSWORD"],
+      authentication:       'plain',
+      enable_starttls_auto: true
+    }
+
   end
 end
